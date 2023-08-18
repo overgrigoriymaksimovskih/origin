e6f9b46304fb7c3d88bac761296e3c1e4f49ffbc
overgrigoriymaksimovskih <overbf@gmail.com>
2023-08-18 14:57:03 +0500

    master commit 4

--- src/TestGIT.java
+++ src/TestGIT.java
@@ -3,5 +3,6 @@ public class TestGIT {
         System.out.println("first change 12:19");
         System.out.println("second change 12:26");
         System.out.println("changes 13:47...");
+        Driver Petya = new Driver("Petya", 22, 4567);
     }
 }
