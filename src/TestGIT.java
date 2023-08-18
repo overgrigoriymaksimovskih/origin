185d88ae894ac436c6f5170accf1b83dae0d8b54
overgrigoriymaksimovskih <overbf@gmail.com>
2023-08-18 13:48:06 +0500

    master commit 3

--- src/TestGIT.java
+++ src/TestGIT.java
@@ -2,6 +2,6 @@ public class TestGIT {
     public static void main(String[] args) {
         System.out.println("first change 12:19");
         System.out.println("second change 12:26");
-        System.out.println("changes 13:47");
+        System.out.println("changes 13:47...");
     }
 }
